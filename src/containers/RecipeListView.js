c0d67905235d6fb37f427ec808c46aa1d4b331c7
Valian <jakub.skalecki@gmail.com>
2017-10-22 10:28:40 +0200

    removed function as a children

--- src/containers/RecipeListView.js
+++ src/containers/RecipeListView.js
@@ -3,28 +3,22 @@ import RecipeItem from "./RecipeItem";
 import Counter from "./Counter";
 import {Button, Header, Item, Segment} from "semantic-ui-react";
 
+// here you should use function as a children, using Counter component
+// to display -, + and amount how many recipes should be displayed
+
 export default ({ recipes = [], recipeRefresh }) =>
-  <Counter>
-    {(counter, plus, minus) => {
-      return (
-        <div>
-          <Segment clearing vertical basic>
-            <Button.Group basic floated="right">
-              <Button onClick={minus} icon="minus" />
-              <Button>limit: {counter}</Button>
-              <Button onClick={plus} icon="plus" />
-              <Button onClick={recipeRefresh} icon="refresh" />
-            </Button.Group>
-            <Header floated="left" as="h1">
-              Dostępne przepisy:
-            </Header>
-          </Segment>
-          <Item.Group divided>
-            {recipes
-              .slice(0, counter)
-              .map(item => <RecipeItem key={item.id} id={item.id} />)}
-          </Item.Group>
-        </div>
-      );
-    }}
-  </Counter>
+    <div>
+      <Segment clearing vertical basic>
+        <Button.Group basic floated="right">
+          <Button onClick={recipeRefresh} icon="refresh" />
+        </Button.Group>
+        <Header floated="left" as="h1">
+          Dostępne przepisy:
+        </Header>
+      </Segment>
+      <Item.Group divided>
+        {recipes
+          .map(item => <RecipeItem key={item.id} id={item.id} />)}
+      </Item.Group>
+    </div>
+
