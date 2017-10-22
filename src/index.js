11814476c9a0111dee275271a02f5487faec019f
Valian <jakub.skalecki@gmail.com>
2017-10-22 11:34:54 +0200

    initial recipes state

--- src/index.js
+++ src/index.js
@@ -20,6 +20,105 @@ const initialState = {
       name: 'Dżabka',
     },
   ],
+  recipes: {
+    order: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7
+    ],
+    entities: {
+      '1': {
+        id: 1,
+        title: 'Młody jaś w pikantnym sosie pomidorowym',
+        ingredients: [
+          'pomidor',
+          'cebula',
+          'fasola jaś',
+          'czosnek',
+          'kiełbasa chorizo',
+          'papryka',
+          'papryczka chili'
+        ]
+      },
+      '2': {
+        id: 2,
+        title: 'Placki owsiane z jabłkami i gruszkami',
+        ingredients: [
+          'płatki owsiane',
+          'jabłko',
+          'gruszka',
+          'mleko',
+          'jajko',
+          'mąka orkiszowa'
+        ]
+      },
+      '3': {
+        id: 3,
+        title: 'Cytrynowe ciasteczka',
+        ingredients: [
+          'mąka ryżowa',
+          'cytryna',
+          'jajka',
+          'masło',
+          'cukier'
+        ]
+      },
+      '4': {
+        id: 4,
+        title: 'Dynioburgery',
+        ingredients: [
+          'pomidor',
+          'papryczka chili',
+          'sałata',
+          'ser żółty',
+          'soczewica',
+          'dynia'
+        ]
+      },
+      '5': {
+        id: 5,
+        title: 'Risotto z chorizo, oliwkami i suszonymi pomidorami',
+        ingredients: [
+          'ryż',
+          'kiełbasa chorizo',
+          'suszone pomidory',
+          'cebula',
+          'ser parmezan',
+          'oliwki'
+        ]
+      },
+      '6': {
+        id: 6,
+        title: 'Muffinki owsiane z borówkami',
+        ingredients: [
+          'borówka',
+          'płatki owsiane',
+          'jajko',
+          'banany',
+          'mleko',
+          'mąka orkiszowa'
+        ]
+      },
+      '7': {
+        id: 7,
+        title: 'Makaron z kurkami i trzema serami',
+        ingredients: [
+          'kurkami',
+          'czosnek',
+          'ser mascarpone',
+          'makaron',
+          'masło',
+          'ser parmezan',
+          'ser kozi'
+        ]
+      }
+    },
+    loading: false
+  },
 };
 
 const store = configureStore(initialState);
